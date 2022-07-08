86a7d1a107a5347a10aa861cb24e69a8a5c38bdc
AllyGarcia <allygarcia152@gmail.com>
2022-07-08 18:18:16 -0500

    added sign up functionality in createaccount.js

--- client/src/pages/CreateAccount.js
+++ client/src/pages/CreateAccount.js
@@ -1,6 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { ADD_USER } from "../utils/mutations";
 
-function CreateAccount() {
+import Auth from '../utils/auth';
+
+const CreateAccount = () => {
+  const [formState, setFormState] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [addUser, { error }] = useMutation(ADD_USER);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await addUser({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.addUser.token);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <div className="grid grid-cols-2 sm:grid-cols-1 w-full bg-cadet md:p-12">
       <div className="md:p-6 sm:p-4 flex flex-wrap justify-center">
@@ -18,23 +51,36 @@ function CreateAccount() {
         </p>
       </div>
       <div className="flex flex-col justify-center sm:p-6">
-        <form className="max-w-[400px] w-full mx-auto bg-antique p-4">
+        <form
+          onSubmit={handleFormSubmit}
+          className="max-w-[400px] w-full mx-auto bg-antique p-4"
+        >
           <h2 className="text-3xl font-bold text-center py-6">
             Welcome to Code_It
           </h2>
           <div className="flex flex-col py-2">
+            <label>username</label>
+            <input onChange={handleChange} className="border p-2" type="text" />
+          </div>
+          <div className="flex flex-col py-2">
             <label>email:</label>
-            <input className="border p-2" type="text" />
+            <input onChange={handleChange} className="border p-2" type="text" />
           </div>
           <div className="flex flex-col py-2">
             <label>password:</label>
-            <input className="border p-2" type="password" />
+            <input
+              onChange={handleChange}
+              className="border p-2"
+              type="password"
+            />
           </div>
-          <button className="border w-full my-5 py-2 bg-mellow">Create Account</button>
+          <button className="border w-full my-5 py-2 bg-mellow">
+            Create Account
+          </button>
         </form>
       </div>
     </div>
   );
-}
+};
 
 export default CreateAccount;
