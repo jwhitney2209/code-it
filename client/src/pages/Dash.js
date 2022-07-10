55968753a81ec26bc69bfcc11af0085629a7aaf9
Jason Whitney <95243766+jwhitney2209@users.noreply.github.com>
2022-07-10 14:10:57 -0500

    Revert "Feature/dash"

--- client/src/pages/Dash.js
+++ client/src/pages/Dash.js
@@ -1,9 +1,6 @@
 import React from "react";
 import Auth from "../utils/auth";
-
-import CategoryForm from "../components/CategoryForm/index";
-import NoteForm from "../components/NoteForm/index";
-
+import { Link } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { QUERY_CATEGORIES, QUERY_ME, QUERY_NOTES } from "../utils/queries";
 
@@ -28,31 +25,27 @@ function Dash() {
   const loggedIn = Auth.loggedIn();
 
   return (
-
-    <main>
-      <div className="flex ">
-        {loggedIn && (
-          <div className="flex-1 block">
-            <CategoryForm />
-          </div>
-        )}
-        <div className={`flex-1 block ${loggedIn}`}>
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <CategoryList categories={categories} title="Select a Category" />
-          )}
+    <div className="grid grid-cols-2 sm:grid-cols-1 w-full bg-cadet md:p-12">
+      <div className="md:p-6 sm:p-4 flex flex-wrap">
+        <div class="flex space-x-2 justify-center">
+          <Link
+            to="/createcategory"
+            type="button"
+            class="border inline-block px-6 py-2.5 bg-mellow text-white font-medium text-xs leading-tight uppercase shadow-md hover:bg-lime hover:shadow-lg focus:bg-mellow focus:shadow-lg focus:outline-none focus:ring-0 active:bg-mellow active:shadow-lg transition duration-150 ease-in-out"
+          >
+            [+] Add a Category
+          </Link>
         </div>
-        {loggedIn && userData ? (
-          <div className="flex-4">
-            <NoteForm
-              username={userData.me.username}
-            />
-          </div>
-        ) : null}
-
+        {/* <div class="flex space-x-2 mx-2 justify-center">
+          <button
+            type="button"
+            class="inline-block px-6 py-2.5 bg-mellow text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-mellow hover:shadow-lg focus:bg-mellow focus:shadow-lg focus:outline-none focus:ring-0 active:bg-mellow active:shadow-lg transition duration-150 ease-in-out"
+          >
+            [+] Add a Note
+          </button>
+        </div> */}
       </div>
-    </main>
+    </div>
   );
 }
 
