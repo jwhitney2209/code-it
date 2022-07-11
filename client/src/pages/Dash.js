377027e98fdef060c114fabaefba74be0f37cadd
teachergg <jwhitney@bisd.us>
2022-07-11 13:07:33 -0500

    push for ally to pull

--- client/src/pages/Dash.js
+++ client/src/pages/Dash.js
@@ -2,16 +2,14 @@ import React from 'react';
 import Auth from '../utils/auth';
 import { useQuery } from '@apollo/client';
 import { Link } from 'react-router-dom';
-import { QUERY_CATEGORIES, QUERY_ME, QUERY_NOTES } from '../utils/queries';
+import { QUERY_ME } from '../utils/queries';
 import CategoryList from '../components/CategoryList';
 import SideBar from '../components/SideBar';
 //import icons
-import { VscNewFolder, VscNotebook } from 'react-icons/vsc';
-import { GiPowerButton } from 'react-icons/gi';
+
 
 function Dash() {
-  const { data } = useQuery(QUERY_CATEGORIES);
-  const { data: userData } = useQuery(QUERY_ME);
+  const { data } = useQuery(QUERY_ME);
 
   const categories = data?.categories || [];
 
@@ -21,9 +19,9 @@ function Dash() {
 
   return (
     <div className="grid grid-cols-4 justify-between h-screen w-full text-antique bg-mellow">
-      {loggedIn && <SideBar />}
       <SideBar />
       <div>Notes Here</div>
+      <CategoryList categories={categories} />
     </div>
   );
 }
