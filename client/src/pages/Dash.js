404eaaaddb609dd17ecd43973376b9dd29b1466c
pixiepucker <autumnoffic@gmail.com>
2022-07-10 20:58:09 -0500

    added noteForm and basic dash styles, deleted CreateNote Page in favor of NoteForm Link

--- client/src/pages/Dash.js
+++ client/src/pages/Dash.js
@@ -1,9 +1,11 @@
-import React from "react";
-import Auth from "../utils/auth";
-import { useQuery } from "@apollo/client";
-import { Link } from "react-router-dom";
-import { QUERY_CATEGORIES, QUERY_ME, QUERY_NOTES } from "../utils/queries";
-import CategoryList from "../components/CategoryList";
+import React from 'react';
+import Auth from '../utils/auth';
+import { useQuery } from '@apollo/client';
+import { Link } from 'react-router-dom';
+import { QUERY_CATEGORIES, QUERY_ME, QUERY_NOTES } from '../utils/queries';
+import CategoryList from '../components/CategoryList';
+//import icons
+import { VscNewFolder } from 'react-icons/vsc';
 
 function Dash() {
   const { data } = useQuery(QUERY_CATEGORIES);
@@ -16,25 +18,24 @@ function Dash() {
   const loggedIn = Auth.loggedIn();
 
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-1 w-full bg-cadet md:p-12">
-      <div className="md:p-6 sm:p-4 flex flex-wrap">
-        <div className="flex space-x-2 justify-center">
-          {loggedIn && (
-            <Link
-              to="/createcategory"
-              type="button"
-              className="border inline-block px-6 py-2.5 bg-mellow text-white font-medium text-xs leading-tight uppercase hover:bg-lime focus:bg-mellow focus:outline-none focus:ring-0 active:bg-mellow transition duration-150 ease-in-out"
-            >
-              [+] Add a Category
-            </Link>
-          )}
-        </div>
+    <div className="flex flex-col h-screen w-full text-antique bg-mellow">
+      {loggedIn && <VscNewFolder />}
+      <div className="flex flex-col h-screen md:w-28 sm:w-20 sm:items-center bg-cadet">
         <div>
+          <Link to="/createcategory" type="button" className="group">
+            <VscNewFolder
+              size={68}
+              className=" hover:bg-lime hover:text-liver hover:rounded-xl bg-mellow rounded-full p-1 md:mx-2 md:mt-6 sm:mx-1 sm:mt-4"
+            />
+            <p className="group-hover:scale-100 absolute w-auto p-2 m-2 min-w-max md:left-24 md:top-24 sm:left-20 sm:top-20 rounded-md shadow-md text-antique bg-liver text-xs font-bold transition-all duration-100 scale-0 origin-left">
+              Add Folder
+            </p>
+          </Link>
         </div>
-        <div>
-          {loggedIn && userData ? (
-           <CategoryList categories={categories} />
-          ) : null}
+
+        <div className="flex flex-wrap items-center md:mx-2 md:p-2">
+          <h5 className="md:text-md">Categories: </h5>
+          <CategoryList categories={categories} />
         </div>
       </div>
     </div>
