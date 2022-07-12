a317980f12f4cba43ae274b35fae5c4bedb1dc06
teachergg <jwhitney@bisd.us>
2022-07-11 21:09:42 -0500

    push for styling on sidebar

--- client/src/pages/Dash.js
+++ client/src/pages/Dash.js
@@ -23,9 +23,14 @@ function Dash() {
     <div>
       {loggedIn && (
         <div className="grid grid-cols-4 justify-between h-screen w-full text-antique bg-mellow">
-          <SideBar />
-          <div>Notes Here</div>
-          <CategoryList categories={categories} />
+          <div
+            class="w-60 h-full bg-cadet/90 px-1 absolute"
+            id="sidenav"
+          >
+            <ul>
+            <SideBar categories={categories}/>
+            </ul>
+          </div>
         </div>
       )}
     </div>
