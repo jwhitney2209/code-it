3c6013e08f99692987021d9d78767f8ce2c2f468
teachergg <jwhitney@bisd.us>
2022-07-13 00:20:43 -0500

    added code block to note display

--- client/src/pages/SingleNote.js
+++ client/src/pages/SingleNote.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, useParams } from 'react-router-dom';
+import { CopyBlock, dracula } from 'react-code-blocks';
 
 import { useQuery } from '@apollo/client';
 import { QUERY_NOTE } from '../utils/queries';
@@ -34,11 +35,18 @@ const SingleNote = props => {
       <div className="flex flex-col justify-center h-screen w-full text-liver bg-mellow">
         <div className="sm:p-6">
           <div className="max-w-[400px] w-full mx-auto bg-antique p-4">
-            <h1>{note.noteTitle}</h1>
-            <h1>{note.noteText}</h1>
-            <h1>{note.noteSnippet}</h1>
-            <h1>{note.tag}</h1>
-            <h1>{note.createdAt}</h1>
+          <h1><strong>Title:</strong></h1>
+            <p>{note.noteTitle}</p>
+            <h1><strong>Description:</strong></h1>
+            <p>{note.noteText}</p>
+            <div>
+              <h1><strong>Code Snippet:</strong></h1>
+              <CopyBlock 
+                text={note.noteSnippet}
+                theme={dracula}
+                />
+            </div>
+            <h1>Created {note.createdAt}</h1>
             <Link
               to="/dashboard"
               className="border w-[6.5rem] inline-block px-6 my-2 py-2.5 bg-mellow hover:bg-lime text-liver font-medium text-xs leading-tight uppercase transition duration-150 ease-in-out"
