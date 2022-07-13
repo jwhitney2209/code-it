7c5e1d515562132796003444dca15fa19d955527
AllyGarcia <allygarcia152@gmail.com>
2022-07-12 23:30:29 -0500

    add styling to dash to reformat

--- client/src/pages/Dash.js
+++ client/src/pages/Dash.js
@@ -1,56 +1,29 @@
-import React, { useState } from 'react';
-import Auth from '../utils/auth';
-import { useQuery } from '@apollo/client';
-import { Link } from 'react-router-dom';
-import { QUERY_ME } from '../utils/queries';
-import CategoryList from '../components/CategoryList';
-import SideBar from '../components/SideBar';
-import NoteList from '../components/NoteList';
-import { VscSearch } from 'react-icons/vsc';
+import React, { useState } from "react";
+import Auth from "../utils/auth";
+import SideBar from "../components/SideBar";
+
+import { useQuery } from "@apollo/client";
+import { Link } from "react-router-dom";
+import { QUERY_ME } from "../utils/queries";
+import CategoryList from "../components/CategoryList";
+import NoteList from "../components/NoteList";
+import { VscSearch } from "react-icons/vsc";
 //import icons
 
 function Dash() {
-  const [searchText, setSearchText] = useState('');
-
   const loggedIn = Auth.loggedIn();
 
   return (
-    <div className="flex justify-between items-center">
-      <div className="h-screen w-full text-liver bg-mellow">
-        <div
-          class="md:w-60 md:h-full sm:w-full bg-cadet/90 px-1 absolute"
-          id="sidenav"
-        >
-          <div className="flex flex-row p-1 md:mx-2 md:mt-3 sm:mx-1 sm:mt-4 align-items: center justify-items-start bg-lime rounded-2xl border">
-            <VscSearch size={20} className="fill-liver pl-1" />
-            <input
-              className="border-none bg-lime pl-2 focus:outline-none caret-liver text-liver"
-              type="text"
-              placeholder="type to search"
-              onChange={setSearchText}
-            />
-          </div>
-          <div className="flex space-x-2 justify-center">
-            <Link
-              to="/createnote"
-              type="button"
-              className="border w-full inline-block px-6 my-2 py-2.5 bg-mellow hover:bg-lime text-liver font-medium text-xs leading-tight uppercase transition duration-150 ease-in-out"
-            >
-              [+] Add Note
-            </Link>
-          </div>
-          {loggedIn && (
-            <ul>
-              <SideBar />
-            </ul>
-          )}
-        </div>
-
+    <main className="flex flex-row items-center min-h-full md:h-full">
+      <div className="basis-1/4 md:w-60 sm:w-full min-h-full">
+        <SideBar />
+      </div>
+      <div className="basis-3/4 md:h-full bg-mellow">
         <h3 className="text-center md:text-3xl sm:text-xl">
           Please select a note to view.
         </h3>
       </div>
-    </div>
+    </main>
   );
 }
 
