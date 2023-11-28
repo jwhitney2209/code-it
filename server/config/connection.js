19afb90d5d8712bac4c2b45235fd7c48e040a405
Jason Whitney <95243766+jwhitney2209@users.noreply.github.com>
2023-11-28 00:13:19 -0600

    added MONGODB_URI environment variable to connection.js that was previously removed

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require("dotenv").config();
 
-mongoose.connect('mongodb://127.0.0.1:27017/codeit_db');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/codeit_db');
 
 module.exports = mongoose.connection;
